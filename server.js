8a2030740e27653ec4d3058f849ef2173a895e8c
Miguel Maletzke <88055057+MiguelMA3@users.noreply.github.com>
2025-12-01 18:25:02 +0000

    documentacao swagger

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const orderRoutes = require('./src/routes/orderRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Jitterbit Orders API',
+            version: '1.0.0',
+            description: 'API CRUD simples para gerenciamento de pedidos.',
+        },
+        servers: [{
+            url: `http://localhost:${PORT}`,
+        }],
+    },
+    
+    apis: ['./src/routes/*.js'], 
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
@@ -16,8 +37,10 @@ mongoose.connect(MONGODB_URI)
     process.exit(1);
   });
 
+
 app.use('/order', orderRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
